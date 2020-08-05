cfa91b9d1813ba914f6a4aa5d509d07b5a20b927
rimni647 <rimni647@github.com>
2020-08-05 18:54:56 +0200

    added walking animation for  down direction

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ pygame.display.set_caption("Pokemon")
 black = 0, 0, 0
 white = 255, 255, 255
 
-player_img = pygame.image.load("images/ash_front_stand.png")
+#player_img = pygame.image.load("images/ash_front_stand.png")
 
-player_position = x, y = 1, 1
+#player_position = x, y = 1, 1
 
 quit_game = False
 
@@ -30,6 +30,29 @@ class player():
         self.x_change = 0
         self.y_change = 0
         self.speed = 0
+        self.player_img = pygame.image.load("images/ash_front_stand.png")
+        self.player_sprites = {
+            "front_stand":"images/ash_front_stand.png",
+            "front_walk_r":"images/ash_front_walk_right.png", 
+            "front_walk_l":"images/ash_front_walk_left.png",
+            "back_stand":"images/ash_back_stand.png",
+            "back_walk_r": "images/ash_back_walk_right.png"}
+        self.direction = 'down'
+        self.foot= 'left'
+    
+    def change_direction(self):
+        if self.direction == 'down':
+            if self.y_change == 0:
+                self.player_img = pygame.image.load(self.player_sprites['front_stand'])
+            elif self.foot == 'left':
+                print(self.foot)
+                self.player_img = pygame.image.load(self.player_sprites['front_walk_l'])
+                self.foot = 'right'
+            elif self.foot == 'right':
+                print(self.foot)
+                self.player_img = pygame.image.load(self.player_sprites['front_walk_r'])
+                self.foot = 'left'
+
 # Game Loop
 ash = player()
 while not quit_game:
@@ -41,13 +64,17 @@ while not quit_game:
                     ############################
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                ash.x_change = -5
+                ash.x_change = -1
+                ash.direction = 'left'
             elif event.key == pygame.K_RIGHT:
-                ash.x_change = 5
+                ash.x_change = 1
             elif event.key == pygame.K_UP:
-                ash.y_change = -5
+                ash.y_change = -1
             elif event.key == pygame.K_DOWN:
-                ash.y_change = 5    
+                ash.y_change = 1  
+                ash.direction = 'down'
+                print(1)
+                ash.change_direction()
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 ash.x_change = 0
@@ -61,7 +88,7 @@ while not quit_game:
     ##
     player_position = ash.x, ash.y
     game_display.fill(white)
-    game_display.blit(player_img, player_position)
+    game_display.blit(ash.player_img, player_position)
   
 
     pygame.display.update()
