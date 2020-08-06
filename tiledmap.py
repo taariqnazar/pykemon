551b466255e3d558ed6a267e21e463456202fd75
tariqnazar <tariq.nazar13@gmail.com>
2020-08-06 15:10:29 +0200

    added camera following and updated movement functions

--- tiledmap.py
+++ tiledmap.py
@@ -7,7 +7,7 @@ class TiledMap:
     def __init__(self, filename):
         tm = pytmx.load_pygame(filename, pixelalpha=True)
         self.width = tm.width * tm.tilewidth
-        self.height = tm.height* tm.tileheight
+        self.height = tm.height * tm.tileheight
         self.tmxdata = tm
 
     def render(self, surface):
@@ -17,12 +17,10 @@ class TiledMap:
                 for x, y, gid, in layer:
                     tile = ti(gid)
                     if tile:
-                        print(tile)
-                        surface.blit(tile, (x * self.tmxdata.tilewidth,y * self.tmxdata.tileheight))
-    
+                        surface.blit(
+                            tile, (x * self.tmxdata.tilewidth, y * self.tmxdata.tileheight))
+
     def make_map(self):
         temp_surface = pg.Surface((self.width, self.height))
         self.render(temp_surface)
         return temp_surface
-
-
